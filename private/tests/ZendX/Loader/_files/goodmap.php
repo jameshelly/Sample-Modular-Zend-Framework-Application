20d5adbb183ad51ad53053c63dad46fe619e8556
James A Helly <mrhelly@gmail.com>
2011-08-20 16:14:27 +0100

    nah, didn't finish tests

--- private/tests/ZendX/Loader/_files/goodmap.php
+++
@@ -1,7 +0,0 @@
-<?php
-$ds       = DIRECTORY_SEPARATOR;
-$basePath = realpath(dirname(__FILE__) . "$ds..");
-return array(
-    'ZendX_Loader_StandardAutoloaderTest' => $basePath . $ds . 'StandardAutoloaderTest.php',
-    'ZendX_Loader_ClassMapAutoloaderTest' => $basePath . $ds . 'ClassMapAutoloaderTest.php',
-);
